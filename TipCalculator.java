5a569409cdb84e552d2512dc26fd2535a7794676
alejandro <cesar.alejandro.fernandez.7@gmail.com>
2023-06-06 18:32:43 -0300

    added: tipCalculator class

--- TipCalculator.java
+++ TipCalculator.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class TipCalculator {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Ingrese el total de la cuenta a pagar: ");
+
+        double totalToPay =  keyboard.nextDouble();
+
+        System.out.print("Ingrese el porcentaje de propina que desea dejar: ");
+
+        double tipsRate =  keyboard.nextDouble();
+
+        double tipsAmount = totalToPay * (tipsRate / 100);
+
+        System.out.println("El monto de la propina a dejar es: " + tipsAmount);
 
     }
 }
